8b1856a4b92f418d65ffeac0de5583f105a6bf74
Ulric <rick.sinclair.nyc@gmail.com>
2020-06-12 15:22:41 -0400

    latest

--- cashbailbackend/src/main/java/com/blacklivesmatter/cashbailbackend/model/Cause.java
+++ cashbailbackend/src/main/java/com/blacklivesmatter/cashbailbackend/model/Cause.java
@@ -4,8 +4,11 @@ import lombok.*;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @NoArgsConstructor
@@ -20,7 +23,9 @@ public class Cause {
     int id;
     String description;
     BigDecimal amountRequired;
-    BigDecimal amountRecieved;
+    BigDecimal amountReceived;
+    LocalDate dateOpened;
+    @ManyToOne
     Organization associatedOrganization;
 
     @OneToMany(targetEntity = Donation.class)
--- cashbailbackend/src/main/java/com/blacklivesmatter/cashbailbackend/model/Organization.java
+++ cashbailbackend/src/main/java/com/blacklivesmatter/cashbailbackend/model/Organization.java
@@ -1,9 +1,13 @@
 package com.blacklivesmatter.cashbailbackend.model;
 
 import lombok.*;
+import org.springframework.boot.autoconfigure.web.ConditionalOnEnabledResourceChain;
 
+import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import java.util.List;
 
 @NoArgsConstructor
 @AllArgsConstructor
@@ -16,6 +20,10 @@ public class Organization {
     @Id
     int id;
     String Name;
+    @Embedded
+    Address address;
+    @OneToMany(targetEntity = Cause.class)
+    List<Cause> causes;
 
 
 }
