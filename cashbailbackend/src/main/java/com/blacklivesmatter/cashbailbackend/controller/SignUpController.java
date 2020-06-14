5ce2f4e757717f3f58e24d8d2cd565edabb4946a
Wade Taylor <wayne.baylor@gmail.com>
2020-06-14 11:52:28 -0400

    connected registration

--- cashbailbackend/src/main/java/com/blacklivesmatter/cashbailbackend/controller/SignUpController.java
+++ cashbailbackend/src/main/java/com/blacklivesmatter/cashbailbackend/controller/SignUpController.java
@@ -1,31 +1,38 @@
 package com.blacklivesmatter.cashbailbackend.controller;
 
+import javax.validation.Valid;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
 import com.blacklivesmatter.cashbailbackend.model.AppUser;
 import com.blacklivesmatter.cashbailbackend.service.AuthService;
+
 import lombok.AllArgsConstructor;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 
 @AllArgsConstructor
 @Controller
 public class SignUpController {
 
+	private final AuthService authService;
 
-private final AuthService authService;
-
-
-
-    @GetMapping("/signup")
-    public String showSignUp(){
-        return "signup";
+    @GetMapping("/register")
+    public String showSignUp(@ModelAttribute("donor") AppUser donor){
+        return "registration";
     }
 
-    @PostMapping("/signup")
-    public String registerUser(AppUser fromForm){
-        AppUser createdUser = authService.signUp(fromForm);
+    @PostMapping("/register")
+    public String registerUser(@Valid @ModelAttribute("donor") AppUser fromForm, BindingResult bindingResult){
+    	if(bindingResult.hasErrors()) {
+    		return "/register";
+    	}
+    	
+        authService.signUp(fromForm);
 
-        return "user with id " + createdUser.getId() + " created";
+        return "redirect:/";
     }
 
 }
